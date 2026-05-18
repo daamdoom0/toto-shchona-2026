bee8bf1bf5fe1e4fd57682567132795e5a0caa29
daamdoom0 <adamrosecpa@gmail.com>
2026-05-18 14:08:45 +0300

    Update PlayerFrames.tsx

--- src/components/background/PlayerFrames.tsx
+++ src/components/background/PlayerFrames.tsx
@@ -67,7 +67,7 @@ export default function PlayerFrames() {
             left: f.x,
             top: f.y,
             transform: `rotate(${f.rotation}deg)`,
-            opacity: 0.18,
+            opacity: 0.31,
             filter: "sepia(20%)",
           }}
         >
